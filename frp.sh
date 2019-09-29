971e1c3d4b9ecb1186668b2019a87d5bf21378cb
longjiayu168 <54384756+longjiayu168@users.noreply.github.com>
2019-09-29 10:26:20 +0800

    Update frp.sh

--- frp.sh
+++ frp.sh
@@ -7,8 +7,4 @@ fi
 tar -zxvf frp_0.28.2_linux_amd64.tar.gz
 cd frp_0.28.2_linux_amd64
 sed -i '2s/127.0.0.1/121.40.180.105/g' frpc.ini
-sed -i 's#/usr/bin/#/home/ubuntu/frp_0.28.2_linux_amd64/#' ./systemd/frpc.service
-sed -i 's#/etc/frp/#/home/ubuntu/frp_0.28.2_linux_amd64/#' ./systemd/frpc.service
-cp ./systemd/frpc.service /lib/systemd/system
-systemctl enable frpc.service
-#./frpc 
+./frpc 
